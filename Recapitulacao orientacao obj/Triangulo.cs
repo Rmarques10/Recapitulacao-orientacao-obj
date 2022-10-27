5ad6758cb4e7a4f5149346cdf4dec631a0cc80e8
Rafael Marques <rafaelmarques.rm86@gmail.com>
2022-10-27 13:12:52 -0300

    Recapitulação

---
+++ Recapitulacao orientacao obj/Triangulo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recapitulacao_orientacao_obj
+{
+    internal class Triangulo
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
